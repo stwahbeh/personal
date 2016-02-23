a1819fab729306f749c1428c3e29b9ae4bf0d80f
Sam Wahbeh <stwahbeh@gmail.com>
2016-02-22 16:55:51 -0800

    got angular to work

--- js/website.js
+++ js/website.js
@@ -1,16 +1,21 @@
+'use strict';
+console.log("greg hay yo");
+
 // Create app
 var myApp = angular.module('myApp',[])
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
-	console.log('my controller');
-	var getData = function() {
-		$http.get('data/info.json').success(function(response){
-		console.log("in get function");	
-		$scope.items = response;
-		console.log($scope.items);
-		})
+	//console.log('my controller');
+	$scope.getData = function() {
+		//console.log('am i getting data?');
+		$http.get('./data/info.json').success(function(data){
+		//console.log("in get function");	
+		console.log(data);
+		$scope.items = data;
+		//console.log($scope.items);
+		});
 	}
-	console.log("getting data");
-	getData();
-	console.log("got data");
+	//console.log("getting data");
+	$scope.getData();
+	//console.log("got data");
 });
